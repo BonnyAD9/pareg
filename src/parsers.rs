79e6cb9ee666600392864649448df086bcb4f1a3
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-04-17 09:43:01 +0200

    Add bool parsers

--- src/parsers.rs
+++ src/parsers.rs
@@ -35,3 +35,46 @@ where
 
     Ok((k, T::from_arg(v)?))
 }
+
+/// Parse bool value in a specific way. If the value of lowercase `arg` is
+/// equal to `t` returns true, if it is equal to `f` returns false and
+/// otherwise returns error.
+pub fn bool_arg<'a>(t: &str, f: &str, arg: &'a str) -> Result<'a, bool> {
+    let lower = arg.to_lowercase();
+    if arg == t {
+        Ok(true)
+    } else if arg == f {
+        Ok(false)
+    } else {
+        Err(ArgError::FailedToParse {
+            typ: "bool",
+            value: lower.into(),
+            msg: Some(format!("Value must be '{t}' or '{f}'").into()),
+        })
+    }
+}
+
+/// Parse bool value in a specific way. If the value of lowercase `arg` is
+/// equal to `t` returns true, if it is equal to `f` returns false and
+/// if it is equal to `n` returns [`None`]. Otherwise returns error.
+pub fn opt_bool_arg<'a>(
+    t: &str,
+    f: &str,
+    n: &str,
+    arg: &'a str,
+) -> Result<'a, Option<bool>> {
+    let lower = arg.to_lowercase();
+    if arg == t {
+        Ok(Some(true))
+    } else if arg == f {
+        Ok(Some(false))
+    } else if arg == n {
+        Ok(None)
+    } else {
+        Err(ArgError::FailedToParse {
+            typ: "bool",
+            value: lower.into(),
+            msg: Some(format!("Value must be '{t}' or '{f}'").into()),
+        })
+    }
+}
